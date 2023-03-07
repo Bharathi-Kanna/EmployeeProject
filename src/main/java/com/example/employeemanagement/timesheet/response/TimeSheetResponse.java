d0332510260a2be9d42835ca4d04dc96f66ecf17
vishnuprakashN <90830491+vishnuprakashN@users.noreply.github.com>
2023-03-07 16:57:02 +0530

    added findall in timesheet

--- src/main/java/com/example/employeemanagement/timesheet/response/TimeSheetResponse.java
+++ src/main/java/com/example/employeemanagement/timesheet/response/TimeSheetResponse.java
@@ -11,10 +11,13 @@ import java.time.LocalDateTime;
 public class TimeSheetResponse {
     private Long Id;
     private Long employeeId;
+    private String employeeName;
+    private String projectName;
     private Long projectId;
     private Long hoursWorked;
     private LocalDateTime logTime;
     private Long totalCost;
+
 
     public TimeSheetResponse(TimeSheet timeSheet){
         this.Id = timeSheet.getId();
--- src/main/java/com/example/employeemanagement/timesheet/service/TimeSheetImpli.java
+++ src/main/java/com/example/employeemanagement/timesheet/service/TimeSheetImpli.java
@@ -1,6 +1,11 @@
 package com.example.employeemanagement.timesheet.service;
 
+import com.example.employeemanagement.employee.entity.Employee;
+import com.example.employeemanagement.employee.repository.EmployeeRepo;
 import com.example.employeemanagement.exception.EntityNotFound;
+import com.example.employeemanagement.project.entity.Project;
+import com.example.employeemanagement.project.repository.ProjectRepo;
+import com.example.employeemanagement.timelogs.response.TimeLogResponse;
 import com.example.employeemanagement.timesheet.entity.TimeSheet;
 import com.example.employeemanagement.timesheet.repository.TimeSheetRepo;
 import com.example.employeemanagement.timesheet.request.TimeSheetRequest;
@@ -8,6 +13,7 @@ import com.example.employeemanagement.timesheet.response.TimeSheetResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,21 +21,43 @@ public class TimeSheetImpli implements TimeSheetService{
 
     @Autowired
     TimeSheetRepo timeSheetRepo;
+
+    @Autowired
+    EmployeeRepo employeeRepo;
+
+    @Autowired
+    ProjectRepo projectRepo;
+
     @Override
     public List<TimeSheetResponse> findAllEntity() {
-        return null;
+        List<TimeSheet> timeSheet = timeSheetRepo.findAll();
+        List<TimeSheetResponse> list = new ArrayList<>();
+        for (TimeSheet e:
+                timeSheet) {
+            TimeSheetResponse timeSheetResponse = new TimeSheetResponse(e);
+            Employee employee = employeeRepo.findById(e.getEmployeeId()).orElseThrow(()-> new EntityNotFound("employee not found"));
+            Project project = projectRepo.findById(e.getProjectId()).orElseThrow(()-> new EntityNotFound("project not found"));
+            timeSheetResponse.setEmployeeName(employee.getName());
+            timeSheetResponse.setProjectName(project.getProjectName());
+            list.add(timeSheetResponse);
+        }
+        return list;
     }
 
     @Override
     public TimeSheetResponse findById(Long Id) {
         TimeSheet timeSheet = timeSheetRepo.findById(Id).orElseThrow(()-> new EntityNotFound("timesheet not found"));
         TimeSheetResponse timeSheetResponse = new TimeSheetResponse(timeSheet);
+        Employee employee = employeeRepo.findById(timeSheetResponse.getEmployeeId()).orElseThrow(()-> new EntityNotFound("employee not found"));
+        Project project = projectRepo.findById(timeSheetResponse.getProjectId()).orElseThrow(()-> new EntityNotFound("project not found"));
+        timeSheetResponse.setEmployeeName(employee.getName());
+        timeSheetResponse.setProjectName(project.getProjectName());
         return timeSheetResponse;
     }
 
     @Override
     public TimeSheetResponse updateEntity(TimeSheetRequest eReq, Long id) {
-        return null;
+       return null;
     }
 
     @Override
