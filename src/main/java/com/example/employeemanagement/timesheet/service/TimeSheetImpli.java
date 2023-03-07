4c5167a2e1446cb5658600548ad53fa25b089f66
vishnuprakashN <90830491+vishnuprakashN@users.noreply.github.com>
2023-03-07 15:10:30 +0530

    updated timesheet

--- src/main/java/com/example/employeemanagement/timesheet/service/TimeSheetImpli.java
+++ src/main/java/com/example/employeemanagement/timesheet/service/TimeSheetImpli.java
@@ -2,9 +2,11 @@ package com.example.employeemanagement.timesheet.service;
 
 import com.example.employeemanagement.timesheet.request.TimeSheetRequest;
 import com.example.employeemanagement.timesheet.response.TimeSheetResponse;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TimeSheetImpli implements TimeSheetService{
     @Override
     public List<TimeSheetResponse> findAllEntity() {
