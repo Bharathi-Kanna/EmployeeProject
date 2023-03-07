88d7c1cd6810a6d079ed09f62d271c94dfa471e8
vishnuprakashN <90830491+vishnuprakashN@users.noreply.github.com>
2023-03-07 15:17:40 +0530

    Update TimeLogs.java

--- src/main/java/com/example/employeemanagement/timelogs/entity/TimeLogs.java
+++ src/main/java/com/example/employeemanagement/timelogs/entity/TimeLogs.java
@@ -15,7 +15,7 @@ public class TimeLogs {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "team_id")
     private Long Id;
 
     @Column(name="project_id")
